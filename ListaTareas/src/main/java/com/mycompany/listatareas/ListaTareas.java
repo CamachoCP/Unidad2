ba0f256e725e8d3dc3b90889105d3ecc973dbfc1
CamachoCP <143622267+CamachoCP@users.noreply.github.com>
2023-09-06 08:37:35 -0600

    Add files via upload

---
+++ ListaTareas/src/main/java/com/mycompany/listatareas/ListaTareas.java
@@ -0,0 +1,28 @@
+
+package com.mycompany.listatareas;
+
+
+public class ListaTareas {
+
+    public static void main(String[] args) {
+        
+    int edda;
+    String nombre="Cesar";  
+    String materia="Fundamentos de programacion";
+    String nombreMascota="Miauricio";
+    String NombreP="Ernesto";
+    String NombreM="Isabel";
+    
+    final int anioNacimiento=2002;
+    
+            String cadenaTexto="cadena de texto";
+            
+            System.out.println("Hola mi nombre es:" + nombre);
+            System.out.println("Hola mi nombre es:" + nombre + "y estudio" + materia);
+            System.out.println("Hola mi nombre es:" + nombre + "mi materia favorita es:" + materia );
+                    System.out.println("Hola mi nombre es:" + nombre + "mi materia favorita es:" + materia + "mi papa se llama:" + NombreP   );
+                            System.out.println("Hola mi nombre es:" + nombre + "mi materia favorita es:" + materia +  "mi papa se llama:" + NombreP + "Mi mama se llama"
+                                    + NombreM);
+    
+    }
+}
